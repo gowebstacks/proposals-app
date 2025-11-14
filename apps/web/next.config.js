fd74234309a2f04a46f70ca52b0bb94ab7a1cf98
nshahidi <nshahidi@webstacks.com>
2025-11-13 20:24:28 -0800

    feat: add permanent redirect from root to webstacks.com

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -10,6 +10,15 @@ const nextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: 'https://www.webstacks.com',
+        permanent: true,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
